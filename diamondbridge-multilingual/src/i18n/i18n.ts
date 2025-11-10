4dacf96b64acb8d5d3238ed765da46e450b344a1
Serhii_Stepanov-Baden-Baden <srg.stepanov@gmail.com>
2025-11-10 07:53:12 +0100

    Create i18n.ts

---
+++ diamondbridge-multilingual/src/i18n/i18n.ts
@@ -0,0 +1,15 @@
+import { translations } from './translations';
+
+export type Language = keyof typeof translations;
+
+export const getTranslation = (lang: Language, key: string): string => {
+  const getNestedValue = (obj: any, path: string[]): any =>
+    path.reduce((acc, part) => acc?.[part], obj);
+
+  const keys = key.split('.');
+  const value = getNestedValue(translations[lang], keys);
+  if (value) return value;
+
+  const fallback = getNestedValue(translations.ru, keys);
+  return fallback || key;
+};
