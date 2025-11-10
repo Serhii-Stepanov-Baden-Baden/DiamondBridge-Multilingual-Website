63d716c233e551c7595611cac7df367495fe803f
Serhii_Stepanov-Baden-Baden <srg.stepanov@gmail.com>
2025-11-10 12:59:57 +0100

    Update vite.config.ts

--- diamondbridge-multilingual/vite.config.ts
+++ diamondbridge-multilingual/vite.config.ts
@@ -2,22 +2,13 @@ import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const isProd = process.env.BUILD_MODE === 'prod';
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-    sourceIdentifierPlugin({
-      enabled: !isProd,
-      attributePrefix: 'data-matrix',
-      includeProps: true,
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
