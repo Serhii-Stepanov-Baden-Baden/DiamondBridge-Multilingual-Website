dc3069b9a8cc0fbfda9bad8932de90a21ba33037
Serhii_Stepanov-Baden-Baden <srg.stepanov@gmail.com>
2025-11-10 01:33:52 +0100

    Update vite.config.ts

--- diamondbridge-multilingual/vite.config.ts
+++ diamondbridge-multilingual/vite.config.ts
@@ -1,12 +1,26 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import sourceIdentifierPlugin from 'vite-plugin-source-identifier';
+
+const isProd = process.env.BUILD_MODE === 'prod';
+
 export default defineConfig({
   base: '/', // 👈 обязательно для Vercel
-  plugins: [react(), sourceIdentifierPlugin({ ... })],
+  plugins: [
+    react(),
+    sourceIdentifierPlugin({
+      enabled: !isProd,
+      attributePrefix: 'data-matrix',
+      includeProps: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: 'dist' // 👈 Vercel ищет именно эту папку
-  }
+    outDir: 'dist', // 👈 Vercel ищет именно эту папку
+  },
 });
